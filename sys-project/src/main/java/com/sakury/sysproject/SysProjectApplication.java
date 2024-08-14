961e7f720a2747c206eba61d01a2492e393be895
Sakury <206842393@qq.com>
2024-08-14 17:37:38 +0800

    初始化项目

---
+++ sys-project/src/main/java/com/sakury/sysproject/SysProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sakury.sysproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysProjectApplication.class, args);
+    }
+
+}
